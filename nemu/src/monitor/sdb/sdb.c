9a3191a003e1cd1d7a766447756e95fe122c57b1
tracer-ics2023 <tracer@njuics.org>
2024-05-30 19:48:29 +0800

    >  compile NEMU S2309W0754 张航铭 Linux Ubuntu-24-04 6.8.0-31-generic #31-Ubuntu SMP PREEMPT_DYNAMIC Sat Apr 20 00:40:06 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  19:48:29 up  6:21,  1 user,  load average: 1.11, 0.91, 0.83

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -48,6 +48,15 @@ static int cmd_c(char *args) {
 }
 
 static int cmd_si(char *args){
+  char *arg = strtok(args, " ");
+  int steps = 0;
+  while(arg != NULL){
+    if(strcmp(arg, "si") == 0){
+      arg = strtok(NULL, " ");
+      steps = *arg;
+      cpu_exec(steps);
+    }
+  }
   return 0;
 }
 
@@ -67,7 +76,7 @@ static struct {
   { "q", "Exit NEMU", cmd_q },
 
   /* TODO: Add more commands */
-  { "si", "Single-Step execution", com_si }
+  { "si", "Single-Step execution", cmd_si }
 };
 
 #define NR_CMD ARRLEN(cmd_table)
